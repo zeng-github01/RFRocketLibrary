662c702aff8dfcea97b88aac57cfb8c61dda9432
zeng-github01 <95841646+zeng-github01@users.noreply.github.com>
2026-06-18 01:29:44 +0800

    Add Color32Wrapper to support PaintColor for vehicle, also fix trunk storage restore

---
+++ Models/Color32Wrapper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RFRocketLibrary.Models
+{
+    [Serializable]
+    public struct Color32Wrapper
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public byte A { get; set; }
+
+        public Color32Wrapper(byte r, byte g, byte b, byte a = 255)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+
+        public Color32Wrapper(Color32 color)
+        {
+            R = color.r;
+            G = color.g;
+            B = color.b;
+            A = color.a;
+        }
+
+        public static implicit operator Color32Wrapper(Color32 color)
+        {
+            return new Color32Wrapper(color.r, color.g, color.b, color.a);
+        }
+
+        public static implicit operator Color32(Color32Wrapper wrapper)
+        {
+            return new Color32(wrapper.R, wrapper.G, wrapper.B, wrapper.A);
+        }
+
+        public static Color32Wrapper Create(Color32 color)
+        {
+            return new Color32Wrapper(color);
+        }
+
+        public Color32 ToColor32() => new Color32(R, G, B, A);
+
+        public override string ToString()
+        {
+            return string.Format("{0:X2}{1:X2}{2:X2}{3:X2}", R, G, B, A);
+        }
+
+        public static bool TryParse(string? s, out Color32Wrapper result)
+        {
+            result = default;
+            if (string.IsNullOrWhiteSpace(s))
+                return false;
+
+            s = s.Trim().Replace("#", string.Empty);
+
+            if (s.Length == 6 || s.Length == 8)
+            {
+                if (uint.TryParse(s, System.Globalization.NumberStyles.HexNumber, null, out uint hex))
+                {
+                    if (s.Length == 6)
+                    {
+                        byte r = (byte)((hex >> 16) & 0xFF);
+                        byte g = (byte)((hex >> 8) & 0xFF);
+                        byte b = (byte)(hex & 0xFF);
+                        result = new Color32Wrapper(r, g, b, 255);
+                    }
+                    else
+                    {
+                        byte r = (byte)((hex >> 24) & 0xFF);
+                        byte g = (byte)((hex >> 16) & 0xFF);
+                        byte b = (byte)((hex >> 8) & 0xFF);
+                        byte a = (byte)(hex & 0xFF);
+                        result = new Color32Wrapper(r, g, b, a);
+                    }
+                    return true;
+                }
+            }
+
+            var split = s.Split(',');
+            if (split.Length >= 3)
+            {
+                if (!byte.TryParse(split[0].Trim(), out var r)) return false;
+                if (!byte.TryParse(split[1].Trim(), out var g)) return false;
+                if (!byte.TryParse(split[2].Trim(), out var b)) return false;
+
+                byte a = 255;
+                if (split.Length == 4 && !byte.TryParse(split[3].Trim(), out a)) return false;
+
+                result = new Color32Wrapper(r, g, b, a);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Models/VehicleWrapper.cs
+++ Models/VehicleWrapper.cs
@@ -26,7 +26,7 @@ namespace RFRocketLibrary.Models
         public List<BarricadeWrapper> Barricades { get; set; } = new();
         public Vector3Wrapper Position { get; set; }
         public QuartenionWrapper Rotation { get; set; }
-        // public QuaternionWrapper Rotation { get; set; }
+        public Color32Wrapper PaintColor { get; set; }
 
         public VehicleWrapper()
         {
@@ -35,7 +35,7 @@ namespace RFRocketLibrary.Models
         public VehicleWrapper(ushort id, uint instanceId, ushort skinId, ushort mythicId, float roadPosition,
             ushort health, ushort fuel, ushort batteryCharge, ulong owner, ulong @group, bool[] tires,
             List<byte[]> turrets, ItemsWrapper trunkItems, List<BarricadeWrapper> barricades, Vector3Wrapper position,
-            QuartenionWrapper rotation)
+            QuartenionWrapper rotation, Color32Wrapper paintColor)
         {
             Id = id;
             InstanceId = instanceId;
@@ -53,6 +53,7 @@ namespace RFRocketLibrary.Models
             Barricades = barricades;
             Position = position;
             Rotation = rotation;
+            PaintColor = paintColor;
         }
 
         public static VehicleWrapper Create(InteractableVehicle vehicle)
@@ -76,13 +77,14 @@ namespace RFRocketLibrary.Models
                 Fuel = vehicle.fuel,
                 BatteryCharge = vehicle.batteryCharge,
                 Id = vehicle.id,
-                TrunkItems = vehicle.trunkItems?.items != null && vehicle.trunkItems.items.Count != 0
+                TrunkItems = vehicle.trunkItems?.getItemCount() > 0
                     ? ItemsWrapper.Create(vehicle.trunkItems)
                     : new ItemsWrapper(),
                 Tires = vehicle.tires?.Select(c => c.isAlive).ToArray() ?? Array.Empty<bool>(),
                 Turrets = vehicleTurret,
                 Group = vehicle.lockedGroup.m_SteamID,
                 Owner = vehicle.lockedOwner.m_SteamID,
+                PaintColor = vehicle.PaintColor
             };
 
             if (!BarricadeManager.tryGetPlant(vehicle.transform, out _, out _, out _, out var region))
@@ -119,7 +121,7 @@ namespace RFRocketLibrary.Models
             // Spawn Vehicle
             var vehicle = VehicleManager.SpawnVehicleV3(GetVehicleAsset(), SkinId, MythicId, RoadPosition, Position.ToVector3(),
                 Rotation.ToQuaternion(), false, false, false, false, Fuel, Health, BatteryCharge,
-                owner, group, Owner != 0, null, byte.MaxValue);
+                owner, group, Owner != 0, null, byte.MaxValue, PaintColor);
             
             // Set Tires
             for (var i = 0; i < (Tires?.Length ?? 0); i++)
@@ -130,7 +132,6 @@ namespace RFRocketLibrary.Models
             vehicle.sendTireAliveMaskUpdate();
 
             // Spawn Trunk Items
-            if (vehicle.trunkItems?.items != null && vehicle.trunkItems.items.Count != 0)
                 foreach (var item in TrunkItems.Items)
                     vehicle.trunkItems.addItem(item.X, item.Y, item.Rotation, item.Item.ToItem());
 
